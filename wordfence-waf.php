91fe4995f14157a3b896c2f2dd88813d54c4d6c9
Polvanov <polvanovv@gmail.com>
2019-12-23 10:57:24 +0600

    change permisions for files

--- wordfence-waf.php
+++ wordfence-waf.php
@@ -1,1 +1,8 @@
 <?php
+// Before removing this file, please verify the PHP ini setting `auto_prepend_file` does not point to this.
+
+if (file_exists('/var/www/html/wordpress.test/wp-content/plugins/wordfence/waf/bootstrap.php')) {
+	define("WFWAF_LOG_PATH", '/var/www/html/wordpress.test/wp-content/wflogs/');
+	include_once '/var/www/html/wordpress.test/wp-content/plugins/wordfence/waf/bootstrap.php';
+}
+?>
