5f7602f4a0419c0cb4a5fe60231f99eb245f24e4
Polvanov <polvanovv@gmail.com>
2020-01-09 18:10:06 +0600

    removal of excess code

--- wp-content/plugins/google-api/google-apis.php
+++ wp-content/plugins/google-api/google-apis.php
@@ -7,10 +7,9 @@
 
 require __DIR__.'/vendor/autoload.php';
 
-//add_action('init', 'getClient');
-
 /**
  * Returns an authorized API client.
+ *
  * @return Google_Client the authorized client object
  */
 function getClient()
@@ -22,38 +21,28 @@ function getClient()
     $client->setAccessType('offline');
     $client->setPrompt('select_account consent');
 
-    // Load previously authorized token from a file, if it exists.
-    // The file token.json stores the user's access and refresh tokens, and is
-    // created automatically when the authorization flow completes for the first
-    // time.
     $tokenPath = __DIR__ . '/token.json';
     if (file_exists($tokenPath)) {
         $accessToken = json_decode(file_get_contents($tokenPath), true);
         $client->setAccessToken($accessToken);
     }
 
-    // If there is no previous token or it's expired.
     if ($client->isAccessTokenExpired()) {
-        // Refresh the token if possible, else fetch a new one.
         if ($client->getRefreshToken()) {
             $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
         } else {
-            // Request authorization from the user.
             $authUrl = $client->createAuthUrl();
             printf("Open the following link in your browser:\n%s\n", $authUrl);
             print 'Enter verification code: ';
             $authCode = trim(fgets(STDIN));
 
-            // Exchange authorization code for an access token.
             $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
             $client->setAccessToken($accessToken);
 
-            // Check to see if there was an error.
             if (array_key_exists('error', $accessToken)) {
                 throw new Exception(join(', ', $accessToken));
             }
         }
-        // Save the token to a file.
         if (!file_exists(dirname($tokenPath))) {
             mkdir(dirname($tokenPath), 0700, true);
         }
@@ -63,11 +52,9 @@ function getClient()
 }
 
 
-// Get the API client and construct the service object.
 $client = getClient();
 $service = new Google_Service_Drive($client);
 
-// Print the names and IDs for up to 10 files.
 $optParams = array(
     'pageSize' => 5,
     'fields' => 'nextPageToken, files(id, name)'
