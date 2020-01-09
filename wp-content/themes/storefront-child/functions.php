e66da99e74fb8f70faf413f973cf15bf2b770a46
Polvanov <polvanovv@gmail.com>
2020-01-09 17:14:49 +0600

    added comments to code

--- wp-content/themes/storefront-child/functions.php
+++ wp-content/themes/storefront-child/functions.php
@@ -2,13 +2,23 @@
 
 add_action('after_setup_theme', 'alt_removes_action');
 
+/**
+ * Delete Actions
+ *
+ */
 function alt_removes_action()
 {
     remove_action('storefront_header', 'storefront_secondary_navigation', 30);
     remove_action('homepage', 'storefront_on_sale_products', 60);
-
 }
 
+/**
+ * Add key to google map api
+ *
+ * @param $api
+ *
+ * @return mixed
+ */
 function my_acf_google_map_api( $api ){
 
     $api['key'] = 'AIzaSyC728jq3e_oIgK3SWWathYg__zGsoTY8EQ';
@@ -29,7 +39,11 @@ function art_homepage_product_categories($arg)
     return $arg;
 }
 
-
+/**
+ * Changes quantity of products on page
+ *
+ * @return int
+ */
 function alter_sf_products_per_page()
 {
     return 8;
@@ -38,6 +52,14 @@ function alter_sf_products_per_page()
 add_filter('storefront_products_per_page', 'alter_sf_products_per_page');
 
 add_filter('loop_shop_per_page', 'new_loop_shop_per_page', 20);
+/**
+ *
+ * Changes quantity product columns
+ *
+ * @param $cols
+ *
+ * @return int
+ */
 function new_loop_shop_per_page($cols)
 {
     $cols = 3;
@@ -45,6 +67,9 @@ function new_loop_shop_per_page($cols)
     return $cols;
 }
 
+/**
+ * Move secondary navigation
+ */
 function art_move_storefront_secondary_navigation()
 {
     echo '<div class="new_menu">';
@@ -53,7 +78,13 @@ function art_move_storefront_secondary_navigation()
 }
 
 add_filter('storefront_register_nav_menus', 'art_added_menu_footer', 10, 1);
-
+/**
+ * Add Menu in Menus list
+ *
+ * @param $menus
+ *
+ * @return mixed
+ */
 function art_added_menu_footer($menus)
 {
     $menus['footer'] = 'Footer menu';
@@ -62,7 +93,9 @@ function art_added_menu_footer($menus)
 }
 
 add_action('storefront_before_footer', 'art_render_menu_footer');
-
+/**
+ * Add menu before footer
+ */
 function art_render_menu_footer()
 {
     if (has_nav_menu('footer')) {
@@ -82,6 +115,10 @@ function art_render_menu_footer()
 }
 
 add_action('widgets_init', 'register_my_widgets');
+/**
+ * Registration custom sidebars
+ *
+ */
 function register_my_widgets()
 {
     register_sidebar(
@@ -111,6 +148,13 @@ function register_my_widgets()
 }
 
 add_filter('cron_schedules', 'alt_cron_interval');
+/**
+ * Create a custom schedule for cron
+ *
+ * @param $schedule
+ *
+ * @return mixed
+ */
 function alt_cron_interval($schedule)
 {
     $schedule['every_3_min'] = [
@@ -121,9 +165,11 @@ function alt_cron_interval($schedule)
     return $schedule;
 }
 
-
-
 add_action('admin_enqueue_scripts', 'atl_adds_custom_styles');
+/**
+ * Add custom styles in admin panel
+ *
+ */
 function atl_adds_custom_styles()
 {
     wp_register_style('admin-style', get_stylesheet_directory_uri().'/admin-style.css');
@@ -132,6 +178,9 @@ function atl_adds_custom_styles()
 
 add_action( 'wp_enqueue_scripts', 'alt_scripts_basic' );
 
+/**
+ * Add custom js scripts
+ */
 function alt_scripts_basic ()
 {
     wp_register_script('custom-script', get_stylesheet_directory_uri().'/js/custom-script.js');
